1357797240f9cb319cdaed64f20bf301803295f5
ricoom <ericmwaniki20@gmail.com>
2024-01-05 00:13:07 +0300

    side bar

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -7,10 +7,15 @@ import { Router, RouterLink } from '@angular/router';
   selector: 'app-category',
   standalone: true,
   imports: [CommonModule,RouterLink],
+
+  
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
+close:boolean=false
+open:boolean=true
+
 categories:any[]=[]
 @Output() categoryEvent=new EventEmitter<string>()
 constructor(private service:ProductServiceService){
@@ -25,5 +30,12 @@ this.categoryEvent.emit(cat);
 console.log(cat)
   }
 
-
+closeSide(){
+this.close=false;
+this.open=true
+}
+openSide(){
+this.open=false
+this.close=true
+}
 }
