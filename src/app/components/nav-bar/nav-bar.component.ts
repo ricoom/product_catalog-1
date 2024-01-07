d3598df716569c750ea377608738bef1f208ec84
ricoom <ericmwaniki20@gmail.com>
2024-01-07 23:00:43 +0300

    mobile first

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { CartComponent } from "../cart/cart.component";
 
 @Component({
     selector: 'app-nav-bar',
     standalone: true,
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.css',
-    imports: [SearchBarComponent]
+    imports: [SearchBarComponent, CartComponent]
 })
 export class NavBarComponent {
 
